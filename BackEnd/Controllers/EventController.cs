e4e2e24e2b62f12334d9f3330c6a7b76a5383d73
Klaus Abram Siimon <klsiim@taltech.ee>
2024-10-03 16:44:39 +0300

    Tegin updatemise ja lisamise

--- BackEnd/Controllers/EventController.cs
+++ BackEnd/Controllers/EventController.cs
@@ -1,4 +1,5 @@
 using BackEnd.Data.Repos;
+using BackEnd.Models.Classes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BackEnd.Controllers
@@ -21,12 +22,32 @@ namespace BackEnd.Controllers
         }
 
         [HttpGet("Id")]
-        public async Task<IActionResult> GetEventhById(int Id){
+        public async Task<IActionResult> GetEventById(int Id){
             var events = await _repo.GetById(Id);
             if (events == null){
                 return NotFound();
             }
             return Ok(events);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEvent([FromBody] Event eventModel){
+            var eventExists = await _repo.GetById(eventModel.Id);
+            if (eventExists != null){
+                return Conflict();
+            }
+            var result = await _repo.AddEvent(eventModel);
+            return CreatedAtAction(nameof(GetEventById), new {id = eventModel.Id}, eventModel);
+        }
+
+        [HttpPut("Id")]
+        public async Task<IActionResult> EditEvent(int Id, [FromBody] Event eventModel){
+            var result = await _repo.UpdateEvent(Id, eventModel);
+            if (result == null){
+                return NotFound();
+            }
+
+            return Ok(result);
         }
 
         
