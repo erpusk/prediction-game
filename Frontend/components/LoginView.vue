caa69ceead26c3ab5d2f902b5e12c82238cf9ed7
Maksim Gubanov <maguba@taltech.ee>
2024-10-22 00:37:23 +0300

    Loodud logimise ja registreerimise komponendid ning lehed, samuti nupud login ja sign up suunavad autentimise lehtede juurde.

---
+++ Frontend/components/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+    <form name="login-form">
+        <UFormGroup label="Username">
+            <UInput class="border rounded-md p-2" type="text"/>
+        </UFormGroup>
+
+        <UFormGroup label="Password">
+            <UInput class="border rounded-md p-2" type="password"/>
+        </UFormGroup>
+        
+        <UButton type="submit">
+            Login
+        </UButton>
+    </form>
+</template>
