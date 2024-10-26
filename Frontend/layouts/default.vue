be8f66e1c4397e634b19da3a3448b49732b411b9
Jasper <jalaus@taltech.ee>
2024-10-26 19:59:55 +0300

    globaalne main page nupp lisatud

--- Frontend/layouts/default.vue
+++ Frontend/layouts/default.vue
@@ -1,52 +1,18 @@
 <template>
-  <NuxtLayout>
-    <button 
-      @click="goToMainPage" 
-      class="fixed top-4 left-4 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700">
-      Main Page
-    </button>
-  </NuxtLayout>
+    <div>
+      <button 
+        @click="goToMainPage" 
+        class="fixed top-4 left-4 p-4 rounded-md flex items-center">
+        <i class="fas fa-home text-gray-500" style="font-size: 28px"></i>
+      </button>
+      <NuxtPage />  
+    </div>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router';
-
 const router = useRouter();
-
 const goToMainPage = () => {
   router.push('/');
 };
 </script>
-
-
-<style scoped>
-  .fixed {
-    position: fixed;
-  }
-  .top-4 {
-    top: 1rem;
-  }
-  .left-4 {
-    left: 1rem;
-  }
-  .bg-blue-500 {
-    background-color: #3490dc;
-  }
-  .hover\:bg-blue-700:hover {
-    background-color: #2779bd;
-  }
-  .text-white {
-    color: white;
-  }
-  .py-2 {
-    padding-top: 0.5rem;
-    padding-bottom: 0.5rem;
-  }
-  .px-4 {
-    padding-left: 1rem;
-    padding-right: 1rem;
-  }
-  .rounded-md {
-    border-radius: 0.375rem;
-  }
-</style>
